0e7ebe5b701bb9341503d9a6c75f933ad80eb6b3
Sonu0407 <sonu.sapri2004@gmail.com>
2026-09-20 19:15:17 +0530

    added dynamic message after first time using of using the calculator

--- PyCalc.py
+++ PyCalc.py
@@ -4,10 +4,12 @@ test = input("You want to calculate? type 'yes' or 'no' ")
 
 isGame = True if test == "yes" else False
 
+gameCount = 0
+
 while (isGame):
     # Task 1 Implement adding in the calculator
     print()
-    print("What Do you want to do?\n"
+    print(f"What Do you want to do {"now" if gameCount > 0 else ""}?\n"
     "Type '1' for Addition\n"
     "Type '2' for Subtraction\n"
     "Type 'stop' to exit")
@@ -20,10 +22,12 @@ while (isGame):
         a = int(input("Enter your first number to 'Add' "))
         b = int(input("Enter your second number to 'Add' "))
         print(a + b)
+        gameCount += 1
     elif type == "2":
         a = int(input("Enter your first number to 'Subtract' "))
         b = int(input("Enter your second number to 'Subtract' "))
         print(a - b)
+        gameCount += 1
     elif type == "stop":
         # isGame is false
         isGame = False
