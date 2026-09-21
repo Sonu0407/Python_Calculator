a482b75d07dd6ce93c04d142d6156667a22f1df9
Sonu0407 <sonu.sapri2004@gmail.com>
2026-09-21 20:01:30 +0530

    Multiply and divide added successfully

--- PyCalc.py
+++ PyCalc.py
@@ -12,6 +12,8 @@ while (isGame):
     print(f"What Do you want to do {"now" if gameCount > 0 else ""}?\n"
     "Type '1' for Addition\n"
     "Type '2' for Subtraction\n"
+    "Type '3' for Multiplication\n"
+    "Type '4' for Division\n"
     "Type 'stop' to exit")
 
     type = input("Please enter your input here ")
@@ -27,6 +29,16 @@ while (isGame):
         a = int(input("Enter your first number to 'Subtract' "))
         b = int(input("Enter your second number to 'Subtract' "))
         print(a - b)
+        gameCount += 1
+    elif type == "3":
+        a = int(input("Enter your first number to 'Multiply' "))
+        b = int(input("Enter your second number to 'Multiply' "))
+        print(a * b)
+        gameCount += 1
+    elif type == "4":
+        a = int(input("Enter your first number to 'Divide' "))
+        b = int(input("Enter your second number to 'Divide' "))
+        print(a / b)
         gameCount += 1
     elif type == "stop":
         # isGame is false
